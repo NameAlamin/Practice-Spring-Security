549bd4c43f646826b0e33b2fb957f42cc5e0f219
Alamin <alamin8740@gmail.com>
2022-06-23 09:19:16 +0600

    add hibernate

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -1,5 +1,27 @@
 package com.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "userss")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    private String name;
 
 }
